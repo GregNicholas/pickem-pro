3626a3da50f0762823d67e792b758409ec8d4cf3
GregNicholas <gregschoenberg@gmail.com>
2023-07-09 14:55:23 -0700

    add signin and signup functions and pages

---
+++ firebase/auth/signin.ts
@@ -0,0 +1,19 @@
+import firebase_app from "../config";
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+
+const auth = getAuth(firebase_app);
+
+export default async function signIn(email: string, password: string): Promise<{
+  result: any;
+  error: any;
+}> {
+    let result = null,
+        error = null;
+    try {
+        result = await signInWithEmailAndPassword(auth, email, password);
+    } catch (e) {
+        error = e;
+    }
+
+    return { result, error };
+}
