3af8587b4ed5485d427946e3d798d1fa86c42121
GregNicholas <gregschoenberg@gmail.com>
2023-07-24 11:38:24 -0700

    add user name and leagues on dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,17 +1,34 @@
 import { useAuthProtection } from "../hooks/useAuthProtection";
+import { useAuthContext } from "../context/AuthContext";
 import logOut from "../firebase/auth/signout";
 import Layout from "../components/Layout";
 
 function Dashboard() {
     const isLoading = useAuthProtection();
+    const { user } = useAuthContext();
+
+    // temp data for leagues
+    const myLeagues = ["NFL Group", "Vikings"]
 
     if (isLoading) {
         return <div>checking user authentication</div>
     }
 
+    console.log(user.displayName)
     return (
         <Layout>
-            <h1>You can see this if you're logged in</h1>
+            <h2>{user.displayName}'s Leagues</h2>
+            {myLeagues.length > 0 
+            ? <ul>
+                {myLeagues.map((league) => {
+                    return <li key={league} className="leagueListItem">{league}</li>
+                })}
+            </ul> 
+            : <p>No leagues yet</p>
+            }
+
+            <h3>Find a league</h3>
+            
             <button onClick={() => logOut()}>Sign Out</button>
         </Layout>
     );
