8239d96c7d456d49b0a20ee646b45392a0f805d2
GregNicholas <gregschoenberg@gmail.com>
2023-08-15 20:33:31 -0700

    move CreateLeague to component

---
+++ components/CreateLeague.tsx
@@ -0,0 +1,72 @@
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { Dispatch, FormEvent, SetStateAction, useState } from "react";
+import { db } from "../firebase/config";
+import { useAuthContext } from "../context/AuthContext";
+
+type CreateLeagueProps = {
+  setUpdatingLeague: Dispatch<SetStateAction<number>>;
+}
+
+export default function CreateLeague({setUpdatingLeague}: CreateLeagueProps) {
+  const [createLeagueName, setCreateLeagueName] = useState('');
+  const { user } = useAuthContext();
+
+  const handleCreateLeague = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("SEARCH: ", createLeagueName, "We need to check if this league exists, if not, create a new league");
+    // Add a new document in collection "cities"
+    const docRef = doc(db, "leagues", createLeagueName);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+        // change to display message to user
+        console.log("league already exists");
+    } else {
+        await setDoc(doc(db, "leagues", createLeagueName), {
+            name: createLeagueName,
+            owner: user.uid,
+            memberIds: [user.uid],
+            members: [{ 
+                        name: user.displayName, 
+                        id: user.uid, 
+                        picks: {
+                            week1: {},
+                            week2: {},
+                            week3: {},
+                            week4: {},
+                            week5: {},
+                            week6: {},
+                            week7: {},
+                            week8: {},
+                            week9: {},
+                            week10: {},
+                            week11: {},
+                            week12: {},
+                            week13: {},
+                            week14: {},
+                            week15: {},
+                            week16: {},
+                            week17: {},
+                            week18: {},
+                        }
+                     }],
+        });
+    }
+    setCreateLeagueName('');
+    setUpdatingLeague(prev => prev + 1);
+}
+
+  return (
+    <>
+    <h3>Create a league</h3>
+    <p>Enter a name for your league here</p>
+    <form onSubmit={handleCreateLeague} className="form">
+        <label htmlFor="createLeagueName">
+            <p>League Name</p>
+            <input onChange={(e) => setCreateLeagueName(e.target.value)} required name="createLeagueName" id="createLeagueName" placeholder="enter league name" value={createLeagueName} />
+        </label>
+        <button type="submit">Create</button>
+        {/* {error && <p className="errorMessage">{error?.message?.replace("Firebase: ", "")}</p>} */}
+    </form>
+    </>
+  )
+}
