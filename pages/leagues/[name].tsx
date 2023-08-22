f0cd030fe1eaa90729bd989500385f2b18bcd965
GregNicholas <gregschoenberg@gmail.com>
2023-08-21 20:20:15 -0700

    remove props from League page because we will use context

--- pages/leagues/[name].tsx
+++ pages/leagues/[name].tsx
@@ -4,10 +4,10 @@ type LeagueProps = {
   name: string;
 }
 
-export default function League() {
+export default function League({name}: LeagueProps) {
   return (
     <Layout>
-      <h1>League page</h1>
+      <h1>{name} league page</h1>
     </Layout>
   )
 }
