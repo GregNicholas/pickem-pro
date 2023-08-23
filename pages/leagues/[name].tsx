9afb6789c6e8021d08bc7834a528a652d8998aff
GregNicholas <gregschoenberg@gmail.com>
2023-08-23 12:32:57 -0700

    add redirect logic to league page

--- pages/leagues/[name].tsx
+++ pages/leagues/[name].tsx
@@ -1,18 +1,31 @@
+import { useState } from "react";
 import Layout from "../../components/Layout/Layout";
 import { useRouter } from "next/router";
 import { useLeagueContext } from "../../context/LeagueContext";
+import { useAuthContext } from "../../context/AuthContext";
 
 export default function League() {
+  const [isLoading, setIsLoading] = useState(true);
   const router = useRouter();
   const { selectedLeague } = useLeagueContext();
+  const { user } = useAuthContext();
+
   console.log({ selectedLeague});
-  if (!selectedLeague) {
+  if (!user) {
+    router.push('/');
+  } else if (!selectedLeague) {
     router.push('/dashboard');
+  } else {
+    setIsLoading(false);
   }
 
   return (
     <Layout>
-      <h1>{selectedLeague?.name} page</h1>
+      {
+        isLoading ? <div>checking user authentication</div>
+        :
+        <h1>{selectedLeague?.name} page</h1>
+      }
     </Layout>
   )
 }
