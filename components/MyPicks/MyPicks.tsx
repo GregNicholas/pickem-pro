dd90fa6e2873e290efb968e1cda201fada7865c6
GregNicholas <gregschoenberg@gmail.com>
2023-08-28 17:28:46 -0700

    move selected week state to mypicks

--- components/MyPicks/MyPicks.tsx
+++ components/MyPicks/MyPicks.tsx
@@ -1,47 +1,58 @@
 import { useState, Dispatch, SetStateAction } from "react";
-import styles from "../../pages/leagues/LeaguePage.module.css";
+import leagueStyles from "../../pages/leagues/LeaguePage.module.css";
+import styles from "./MyPicks.module.css";
 
 interface MyPicksProps {
   weeks: string[];
-  pickWeek: string;
-  setPickWeek: Dispatch<SetStateAction<string>>;
   matchups: any;
   myPicks: any;
 }
 
-export default function MyPicks({weeks, pickWeek, setPickWeek, matchups, myPicks}: MyPicksProps) {
+export default function MyPicks({weeks, matchups, myPicks}: MyPicksProps) {
   const CURRENT_TIME_IN_SECONDS = Math.floor(new Date().getTime() / 1000);
-  console.log("MY PICKS: ", matchups)
-  console.log(myPicks, pickWeek)
+  // console.log("MY PICKS: ", myPicks)
+  // console.log(matchups, pickWeek)
+  const [pickWeek, setPickWeek] = useState("week01");
+  const [thisWeekPicked, setThisWeekPicked] = useState(myPicks[pickWeek]);
+
+  console.log("THIS week picks: ", thisWeekPicked);
   return (
     <>
-      <h2 className={styles.subHeader}>My Picks</h2>
-      <label htmlFor="week-select">Choose a week:</label>
-      <select name="weeks" id="week-select" value={pickWeek} onChange={(e) => setPickWeek(e.target.value)}>
-        <option value="">select week</option>
-        {weeks.map((week) => (
-          <option key={week} value={week}>{week}</option>
-        ))}
-      </select>
-
+      <h2 className={leagueStyles.subHeader}>My Picks</h2>
+      <section className={styles.selectsection}>
+        <label htmlFor="week-select">Choose a week:</label>
+        <select name="weeks" id="week-select" value={pickWeek} onChange={(e) => setPickWeek(e.target.value)}>
+          <option value="">select week</option>
+          {weeks.map((week) => (
+            <option key={week} value={week}>{week}</option>
+          ))}
+        </select>
+      </section>
 {/* display user's picks for the selected week */}
       {(pickWeek && myPicks[pickWeek] && Object.keys(myPicks[pickWeek]).length > 0) && myPicks[pickWeek] && Object.keys(myPicks[pickWeek]).map((gameNo) => {
         return <p key={gameNo}>myPicks[pickWeek][gameNo]</p>
       })}
 
 {/* show matchups to pick for selected week */}
-      {(pickWeek && matchups[pickWeek] && Object.keys(matchups[pickWeek]).length > 0) && Object.keys(matchups[pickWeek]).sort().map((matchup) => {
-        console.log("MATCHUP: ", matchup)
+      {matchups && <div className={styles.pickscontainer}>
+        {(pickWeek && matchups[pickWeek] && Object.keys(matchups[pickWeek]).length > 0) && Object.keys(matchups[pickWeek]).sort().map((matchup) => {
+          // check if deadline has passed
+          // (matchups[pickWeek][matchup].time.seconds > CURRENT_TIME_IN_SECONDS);
+          let pickstyle = '';
+          let isBlockedStyle = "blocked";
+          const homeTeam = matchups[pickWeek][matchup].home;
+          const awayTeam = matchups[pickWeek][matchup].away;
 
-        // check if deadline has passed
-        
-        console.log(matchups[pickWeek][matchup].time.seconds > CURRENT_TIME_IN_SECONDS);
+          // if ()
 
-        return <div key={matchups[pickWeek][matchup].home}>
-            <span>{matchups[pickWeek][matchup].away}</span>-@-
-            <span>{matchups[pickWeek][matchup].home}</span>
-          </div>
-      })}
+          // this is the display for each game
+          return <div className={styles.gamecontainer} key={homeTeam}>
+              <div className={styles.teamcontainer}>{awayTeam}</div>@
+              <div className={styles.teamcontainer}>{homeTeam}</div>
+            </div>
+        })}
+      </div>
+      }
     </>
   )
 }
