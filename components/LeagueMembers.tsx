f123699a170fdb1d23096d2c5a071af7ff15675a
GregNicholas <gregschoenberg@gmail.com>
2023-08-29 20:11:34 -0700

    league found modal should come up even if user is a member

--- components/LeagueMembers.tsx
+++ components/LeagueMembers.tsx
@@ -1,27 +1,8 @@
-import { DocumentData } from "firebase/firestore"
 import styles from "../pages/leagues/LeaguePage.module.css"
 import {League} from "../types";
 
-// interface Member {
-//   id: string;
-//   name: string;
-//   picks: {
-//     [week: string]: any; // You can replace 'any' with the specific type if known
-//   };
-// }
-
-// interface LeagueData {
-//   memberIds: string[];
-//   members: {
-//     [memberId: string]: Member;
-//   };
-//   name: string;
-//   owner: string;
-// }
-
 export default function LeagueMembers({leagueData}: {leagueData: League}) {
   const memberNames = Object.entries(leagueData.members).map(([key, data]) => {
-    console.log(key, data)
     return <li key={data.id}>{data.name}</li>
   });
   return (
@@ -29,9 +10,6 @@ export default function LeagueMembers({leagueData}: {leagueData: League}) {
     <h2 className={styles.subHeader}>Members</h2>
     <ul id="members">
       {memberNames}
-      {/* {leagueData?.members.map((member) => {
-        return <li key={member.id}>{member.name}</li>
-      })} */}
     </ul>
   </>
   )
