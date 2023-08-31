63d792e5b7771fbec86fe7bbbd18a865bc64910b
GregNicholas <gregschoenberg@gmail.com>
2023-08-31 16:30:49 -0700

    working out calculations for stats

--- types.ts
+++ types.ts
@@ -18,3 +18,29 @@ export interface League {
   name: string;
   owner: string;
 }
+
+export interface Matchup {
+  winner: string;
+  home: string;
+  away: string;
+}
+
+export interface Picks {
+  [week: string]: {
+    [game: string]: string;
+  };
+}
+
+export interface UsersPicks {
+  [userId: string]: Picks;
+}
+
+export interface LeagueData {
+  members: UsersPicks;
+}
+
+export interface MatchupsData {
+  [week: string]: {
+    [game: string]: Matchup;
+  };
+}
